b54624f6ba8a998eed0fcfefe08c0a7051e77c4d
Soichi Hayashi <hayashis@iu.edu>
2018-01-05 17:01:05 +0000

    set specific dipy version to install

--- build.sh
+++ build.sh
@@ -1,22 +1,7 @@
-
-
+#!/bin/bash
 docker build -t brainlife/dipy .
 
-#brainlife/dipy is auto build
 #if [ $? -eq 0 ];
 #then
 #    docker push brainlife/dipy
-#    #docker push brainlife/dipy:0.13
 #fi
-
-if [ $? -eq 0 ];
-then
-    docker push brainlife/dipy
-
-#    docker run \
-#        -v /var/run/docker.sock:/var/run/docker.sock \
-#        -v `pwd`:/output \
-#        --privileged -t --rm \
-#        singularityware/docker2singularity \
-#        brainlife/dipy
-fi
