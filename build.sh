559d1d7080e5cc726c183e4041b33082668b8d18
Soichi Hayashi <hayashis@iu.edu>
2018-04-06 14:27:03 +0000

    adding build.sh to .gitignore

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -t brainlife/dipy . && docker tag brainlife/dipy brainlife/dipy:0.14.0 && docker push brainlife/dipy
+#docker build -t brainlife/dipy . && docker tag brainlife/dipy brainlife/dipy:0.14.0 && docker push brainlife/dipy
+docker build -t brainlife/dipy .
